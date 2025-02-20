ad4f3a22281e52cc424c876be2a8701ff666d8fb
Sílvio Silva <gasparsilvio7@gmail.com>
2025-02-19 23:31:53 -0500

    Refactor: Reduced and improved code

--- public/assets/script/Engineer.js
+++ public/assets/script/Engineer.js
@@ -10,30 +10,15 @@ $(document).ready(function () {
       data: data,
       success: function (response) {
         let responseJSON = JSON.parse(response);
-        console.log(responseJSON);
-        if (responseJSON.status === 'success') {
-          $('#formEngineer')[0].reset();
-          $('#exampleModal').modal('hide');
-          Swal.fire({
-            title: 'Sucesso!',
-            text: responseJSON.message,
-            icon: 'success',
-            confirmButtonText: 'OK'
-          });
-           setTimeout(function () {
-            location.reload();
-          }, 1200);
-        } else {
-          Swal.fire({
-            title: 'Erro!',
-            text: responseJSON.message,
-            icon: 'error',
-            confirmButtonText: 'OK'
-          });
-           setTimeout(function () {
-            location.reload();
-          }, 1200);
-        }
+        Swal.fire({
+          title: responseJSON.status === 'success' ? 'Sucesso!' : 'Erro!',
+          text: responseJSON.message,
+          icon: responseJSON.status,
+          confirmButtonText: 'OK'
+        });
+        setTimeout(function () {
+          location.reload();
+        }, 1200);
       }
     });
   }
